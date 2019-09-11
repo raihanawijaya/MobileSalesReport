e63549df26d4f240edac574b041176cd1364160e
alvinsyahfrilnurfaiz <alvinnurfaiz9@gmail.com>
2019-09-11 21:13:49 +0700

    Menambahkan AddTransactionActivity

---
+++ app/src/main/java/com/telkom/mobilesalesreport/AddTransactionActivity.java
@@ -0,0 +1,13 @@
+package com.telkom.mobilesalesreport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
