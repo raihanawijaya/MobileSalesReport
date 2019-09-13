9d5a898135d3700a3f4651ed357fa340af599118
alvinsyahfrilnurfaiz <alvinnurfaiz9@gmail.com>
2019-09-13 19:50:55 +0700

    Menambahkan Edit Transaction Activity

---
+++ app/src/main/java/com/telkom/mobilesalesreport/EditTransactionActivity.java
@@ -0,0 +1,13 @@
+package com.telkom.mobilesalesreport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_transaction);
+    }
+}
