3628f3080f200b8f5cbd991a4b58f7d3b90e0c9f
alvinsyahfrilnurfaiz <alvinnurfaiz9@gmail.com>
2019-09-26 16:33:08 +0700

    Menambahkan image Next, tambah, previous

--- app/src/main/java/com/telkom/mobilesalesreport/LoginActivity.java
+++ app/src/main/java/com/telkom/mobilesalesreport/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.telkom.mobilesalesreport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btn_login);
+
+
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
